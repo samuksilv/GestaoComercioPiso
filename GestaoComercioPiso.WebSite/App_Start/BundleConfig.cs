f6e09dfc619ffd0fe23013a35bd744c846f38385
samuksilv <samuel.silv.carvalho@gmail.com>
2017-07-12 13:31:23 -0300

    12/07- 13:31

--- GestaoComercioPiso.WebSite/App_Start/BundleConfig.cs
+++ GestaoComercioPiso.WebSite/App_Start/BundleConfig.cs
@@ -8,26 +8,36 @@ namespace GestaoComercioPiso.WebSite
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/js/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/js/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            #region [bootstrap]
 
-            bundles.Add(new StyleBundle("~/Content/css")
-                .Include( "~/Content/bootstrap.css")
+            bundles.Add(new StyleBundle("~/css/bootstrap")
+               .Include("~/Content/bootstrap.css")
+            );
+
+           bundles.Add(new ScriptBundle("~/js/bootstrap")
+                .Include( "~/Scripts/bootstrap.js")
+                .Include( "~/Scripts/respond.js")
+           );
+
+            #endregion
+
+            bundles.Add(new StyleBundle("~/css/site")
                 .Include("~/Content/site.css")
                 .Include("~/Content/bootstrap-theme.css")
-                );
+            );
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
